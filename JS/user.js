126ac34c302aa8ab57f54b3ce1cf8ba7ebb93410
GPMonkey <63848930+jhwq123@users.noreply.github.com>
2021-03-30 12:57:25 +0900

    Study_Web File Update
    기존 HTML_CSS_JS 공부한거 올림

---
+++ JS/user.js
@@ -0,0 +1,75 @@
+var user = [
+    {
+      "_id": "5f8d4fde5b4d9db6bca81248",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Sadie Oliver",
+      "gender": "female",
+      "email": "sadieoliver@mobildata.com",
+      "phone": "+1 (851) 570-3556"
+    },
+    {
+      "_id": "5f8d4fde9a0820f74db311d6",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Thompson Miles",
+      "gender": "male",
+      "email": "thompsonmiles@mobildata.com",
+      "phone": "+1 (842) 466-3683"
+    },
+    {
+      "_id": "5f8d4fde1a84baa0b7d25697",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Veronica Carey",
+      "gender": "female",
+      "email": "veronicacarey@mobildata.com",
+      "phone": "+1 (840) 404-3450"
+    },
+    {
+      "_id": "5f8d4fde124845763d0dedd5",
+      "age": 24,
+      "eyeColor": "blue",
+      "name": "Lawrence Gomez",
+      "gender": "male",
+      "email": "lawrencegomez@mobildata.com",
+      "phone": "+1 (883) 515-2072"
+    },
+    {
+      "_id": "5f8d4fde2331203a1aa21db4",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Rodriguez Stein",
+      "gender": "male",
+      "email": "rodriguezstein@mobildata.com",
+      "phone": "+1 (801) 402-2082"
+    },
+    {
+      "_id": "5f8d4fde87f9af3fe243ccff",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Keisha Rogers",
+      "gender": "female",
+      "email": "keisharogers@mobildata.com",
+      "phone": "+1 (969) 425-2527"
+    },
+    {
+      "_id": "5f8d4fdea357b8df0558f5d3",
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Jeri Page",
+      "gender": "female",
+      "email": "jeripage@mobildata.com",
+      "phone": "+1 (955) 528-3279"
+    },
+    {
+      "_id": "5f8d4fde17ec9a9bf5a2694b",
+      "age": 39,
+      "eyeColor": "brown",
+      "name": "Terrie Roy",
+      "gender": "female",
+      "email": "terrieroy@mobildata.com",
+      "phone": "+1 (887) 538-2022"
+    }
+  ]
+
